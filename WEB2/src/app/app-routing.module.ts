6a31835fdbe407d2fe7c930d6ec2e0148faf6d4c
n-miletic <nevena.z.miletic@gmail.com>
2020-04-10 13:21:58 +0200

    Navigation bar routing

--- WEB2/src/app/app-routing.module.ts
+++ WEB2/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UnregisteredViewComponent } from './unregistered-view/unregistered-view.component';
+import { SearchResultViewComponent } from './search-result-view/search-result-view.component';
 
 
 const routes: Routes = [
   {
     path:"",
     component: UnregisteredViewComponent
+  },
+  {
+    path:"flights",
+    component: SearchResultViewComponent
+  },
+  {
+    path:"rentacar",
+    component: SearchResultViewComponent
   }
 ];
 
@@ -15,3 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [SearchResultViewComponent, UnregisteredViewComponent]
--- WEB2/src/app/app.module.ts
+++ WEB2/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { UnregisteredViewComponent } from './unregistered-view/unregistered-view.component';
-import { SearchResultViewComponent } from './search-result-view/search-result-view.component';
 import { CompanyViewComponent } from './company-view/company-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    UnregisteredViewComponent,
-    SearchResultViewComponent,
-    CompanyViewComponent
+    CompanyViewComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
