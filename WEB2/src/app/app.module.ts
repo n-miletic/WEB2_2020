bc1518647d71dae0c4fb5feaded636534fe6f325
n-miletic <nevena.z.miletic@gmail.com>
2020-04-15 15:53:12 +0200

    Initial refactoring of Components

--- WEB2/src/app/app.module.ts
+++ WEB2/src/app/app.module.ts
@@ -3,18 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LogInComponent } from './log-in/log-in.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    routingComponents,
-    LogInComponent,
-    SignInComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
