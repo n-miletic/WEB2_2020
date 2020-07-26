74465924ddc262a374f293be031e39a7f10a5e76
Rumenovic <djordje.rumenovic@gmail.com>
2020-07-26 16:26:19 +0200

    preparation of rites

--- WEB2/src/app/entities/car/car.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Car } from './car';
-
-describe('Car', () => {
-  it('should create an instance', () => {
-    expect(new Car()).toBeTruthy();
-  });
-});
--- WEB2/src/app/entities/flight/flight.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Flight } from './flight';
-
-describe('Flight', () => {
-  it('should create an instance', () => {
-    expect(new Flight()).toBeTruthy();
-  });
-});
